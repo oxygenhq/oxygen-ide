26c8648841bab4326f43bbf9369c301cdf3f1b90
N. Dimer <ndimer@cloudbeat.io>
2018-11-30 15:40:08 +0200

    Add initial Jest-based unit tests.

---
+++ test/unit/renderer/store/recorder.spec.js
@@ -0,0 +1,45 @@
+import configureStore from '../../../../app/renderer/store/configureStore';
+import * as actions from '../../../../app/renderer/store/recorder/actions';
+
+function setupStore() {
+  return configureStore.configureStore();
+}
+
+describe('Store: recorder', () => {
+  let store = null;
+  beforeAll(() => {
+    store = setupStore();
+  });
+  afterAll(() => { 
+    store = null;
+  });
+  describe('Init state', () => {
+    let state = null;
+    beforeAll(() => {
+      state = store.getState().recorder;
+    });    
+    it('should have "isRecording" property equal to "false"', () => {
+      expect(state.isRecording).toEqual(false);
+    });
+    it('should have "activeFile" property equal to "null"', () => {
+      expect(state.activeFile).toBeNull();
+    });
+    it('should have "steps" property to be an array', () => {
+      expect(state.steps).toEqual([]);
+    });
+  });
+  describe('Actions - startRecorder', () => {
+    let state = null;
+    beforeAll(() => {
+      let newState = store.dispatch(actions.startRecorder());
+      console.dir(newState);
+      state = store.getState().recorder;      
+    }); 
+    it('should have "isRecording" property equal to "true"', () => {
+      setTimeout(() => {
+        expect(state.isRecording).toEqual(true);
+      }, 1000)
+      
+    });   
+  });
+});
