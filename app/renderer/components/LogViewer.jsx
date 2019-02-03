720156e1336b2e84b22a20c7147e14e98063cf11
Oleg Savka <savkaoleg1996@gmail.com>
2019-02-03 22:17:47 +0200

    task 262

--- app/renderer/components/LogViewer.jsx
+++ app/renderer/components/LogViewer.jsx
@@ -8,49 +8,68 @@
  */
 // @flow
 /* eslint-disable react/no-unused-state */
-import { Icon, Select, Input, message } from 'antd';
+/* eslint-disable ident */
 import React, { PureComponent, Fragment } from 'react';
+import _ from 'underscore';
 import { type LogEntry } from '../types/LogEntry';
 import ScrollContainer from './ScrollContainer';
 
 type Props = {
     logs: Array<LogEntry>,
     category: string,
+    height: number
 };
-  
-export default class LogViewer extends PureComponent<Props> {
-    props: Props;
 
-    render() {
-        const { height, logs, category } = this.props;
-        const lines = logs ? logs.map(log => {
-            return log.message;
-        }) : [];
-        return (
-            <div className="logs-container">
-                <ScrollContainer
-                    disableHorizontal
-                    classes="scroller"
-                >
-                    {() => (
-                        <div
-                            className="logger-textarea"
-                            style={{
-                                height: height - 40,
-                                minHeight: height - 39,
-                            }}
-                        >
-                            { lines.map((line, index) => 
-                                <Fragment key={ `log-${category}-line-${index}`}>
-                                    <pre style={{marginBottom:'0px',whiteSpace:'pre-wrap'}}>{line}</pre>
-                                </Fragment>
-                            )}
-                        </div>
-                    )}
-                </ScrollContainer>
-            </div>
-        );
+export default class LogViewer extends PureComponent<Props> {
+  constructor(props: Props) {
+    super(props: Props);
+    this.state = {
+      refreshScroll: false
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const diff = _.difference(nextProps.logs, this.props.logs);
+    if (diff && diff.length) {
+      this.setState({ refreshScroll: !this.state.refreshScroll });
     }
+  }
+
+  render() {
+    const { height, logs, category } = this.props;
+    const { refreshScroll } = this.state;
+    const lines = logs ? logs.map(log => {
+      return {
+        message: log.message,
+        timestamp: log.timestamp
+      };
+    }) : [];
+    return (
+      <div className="logs-container">
+        <ScrollContainer
+          refreshScroll={refreshScroll}
+          disableHorizontal
+          classes="scroller"
+        >
+          {() => (
+            <div
+              className="logger-textarea"
+              style={{
+                height: height - 40,
+                minHeight: height - 39,
+              }}
+            >
+              { lines.map((line) => (
+                <Fragment key={`log-${category}-line-${line.timestamp}`}>
+                  <pre style={{ marginBottom: '0px', whiteSpace: 'pre-wrap' }}>{line.message}</pre>
+                </Fragment>
+              ))}
+            </div>
+          )}
+        </ScrollContainer>
+      </div>
+    );
+  }
 }
 /*
 <textarea
