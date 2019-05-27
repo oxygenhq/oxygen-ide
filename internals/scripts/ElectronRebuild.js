5ddf1a7732d3ff7029f27606c7f99b071b5128e5
romovs <romovs@gmail.com>
2019-05-27 20:40:02 +0300

    Remove fibers and deasync binaries before rebuilding

--- internals/scripts/ElectronRebuild.js
+++ internals/scripts/ElectronRebuild.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { execSync } from 'child_process';
 import fs from 'fs';
 import dependencies from '../../app/package.json';
+import rimraf from 'rimraf';
 
 const nodeModulesPath =
   path.join(__dirname, '..', '..', 'app', 'node_modules');
@@ -14,6 +15,10 @@ if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(nodeModulesPath)
   const cmd = process.platform === 'win32'
     ? electronRebuildCmd.replace(/\//g, '\\')
     : electronRebuildCmd;
+
+  // cleanup everything in fibers and deasync bin folders. see relevant entry in Confluence for more details.
+  rimraf.sync(path.join(nodeModulesPath, 'fibers', 'bin'));
+  rimraf.sync(path.join(nodeModulesPath, 'deasync', 'bin'));
 
   execSync(cmd, {
     cwd: path.join(__dirname, '..', '..', 'app')
