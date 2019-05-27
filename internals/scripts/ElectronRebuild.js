83e677c3e5d3af430923a1bf9384b0a08e2483ad
romovs <romovs@gmail.com>
2019-05-27 21:51:00 +0300

    Fix fibers build on linux

--- internals/scripts/ElectronRebuild.js
+++ internals/scripts/ElectronRebuild.js
@@ -4,6 +4,8 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 import dependencies from '../../app/package.json';
 import rimraf from 'rimraf';
+import { locateElectronPrebuilt } from 'electron-rebuild/lib/src/./electron-locater';
+import nodeAbi from 'node-abi';
 
 const nodeModulesPath =
   path.join(__dirname, '..', '..', 'app', 'node_modules');
@@ -23,4 +25,14 @@ if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(nodeModulesPath)
   execSync(cmd, {
     cwd: path.join(__dirname, '..', '..', 'app')
   });
+  
+  // on linux, append "glibc" suffix to the Fibers binaries folder. see relevant entry in Confluence for more details.
+  if (process.platform === 'linux') {
+    const electronPath = locateElectronPrebuilt();
+    const electronVersion = require(path.join(electronPath, 'package.json')).version;
+    const abi = nodeAbi.getAbi(electronVersion, 'electron');
+    const fibersOrinal = path.join(nodeModulesPath, 'fibers', 'bin', process.platform + '-' + process.arch + '-' + abi);
+    const fibersFinal = path.join(nodeModulesPath, 'fibers', 'bin', process.platform + '-' + process.arch + '-' + abi + '-glibc');
+    fs.renameSync(fibersOrinal, fibersFinal);
+ }
 }
