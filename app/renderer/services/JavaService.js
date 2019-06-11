c8fcf1786e960221b45ad5bea6a27cd7dd4cf289
Savka Oleg <savkaoleg1996@gmail.com>
2019-06-11 14:21:46 +0300

    windows part

--- app/renderer/services/JavaService.js
+++ app/renderer/services/JavaService.js
@@ -11,88 +11,6 @@ export default class JavaService {
   constructor(store) {
     // console.log('~JavaService elive');
   }
-  
-
-  chromeVersion(){
-    console.log('chromeVersion');
-
-    if (os.platform() === 'win32') {
-      try {
-        const process = require('child_process');   
-        const spawn = process.spawn(path.resolve(__dirname, 'services', 'get_chrome_versions.bat'));
-        spawn.on('error', function(err){
-          console.log('QQ error', err);
-        });
-        spawn.stdout.on('data', function (data) {
-          console.log('QQ ',data);
-          console.log('QQ ',data.toString());
-          console.log('QQ ',data.toString().split('\n'));
-  
-          try {
-            let cmdOut = data.toString().split('\n');
-            let infoArray;
-  
-            if(Array.isArray(cmdOut)){
-              infoArray = cmdOut.filter(function (el) {
-                console.log('el', el);
-                console.log('el.length', el.length);
-                return el.length > 1;
-              });
-            }
-  
-            console.log('QQ infoArray',infoArray);
-  
-            if(Array.isArray(infoArray) && infoArray.length === 6){
-              const lineWithVersion = infoArray['3'];
-              const lineWithVersionSplit = lineWithVersion.split(' ');
-              console.log('lineWithVersion', lineWithVersion);
-              console.log('lineWithVersionSplit', lineWithVersionSplit);
-  
-              if(Array.isArray(lineWithVersionSplit) && lineWithVersionSplit.length){
-                alert('Chrome version '+ lineWithVersionSplit[lineWithVersionSplit.length - 1]);
-              }
-            }
-            
-          } catch(e){
-            console.log('QQ e', e);
-          }
-        });
-        // spawn.stderr.on('data', function (data) {
-        //   console.log('QQ ',data);
-        //   console.log('QQ ',data.toString());
-        // });
-        spawn.on('close', function (code) {
-          if (code == 0)
-                console.log('QQ Stop');
-          else
-                console.log('QQ Start');
-        });
-      } catch(e){
-        console.log('QQ e', e);
-      }
-    } else {
-      const spawn = require('child_process').spawn('/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome', ['--version']);
-      spawn.on('error', function(err){
-        console.log('QQ error', err);
-      });
-
-      spawn.stdout.on('data', function (data) {
-        console.log('QQ ',data);
-        console.log('QQ ',data.toString());
-        console.log('QQ ',data.toString().split('\n'));
-
-        let cmdOut = data.toString().split('\n');
-
-        if(cmdOut && cmdOut[0]){
-          const ArrFromStrWithChromeVersion = cmdOut[0].trim().split(' ');
-          console.log('QQ ArrFromStrWithChromeVersion',ArrFromStrWithChromeVersion);
-          if(Array.isArray(ArrFromStrWithChromeVersion) && ArrFromStrWithChromeVersion.length && ArrFromStrWithChromeVersion.length === 3){
-            alert('Chrome version '+ ArrFromStrWithChromeVersion[2]);
-          }
-        }
-      });
-    }
-  }
 
   javaversion(callback) {
     try {
@@ -130,7 +48,6 @@ export default class JavaService {
     this.store = store;
     this.action$ = action$;
     
-    this.chromeVersion();
     this.javaversion(function(err,version){    
       if(version && typeof version === 'string' && version.startsWith('1.8')){
         // do nothing, java version is correct;
