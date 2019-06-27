31ed95dabdc61aced146421ffacddfc72d21c57b
Savka Oleg <savkaoleg1996@gmail.com>
2019-06-27 13:16:53 +0300

    OI-385 Script content disapears when direcotry is modified (#51)

--- app/main/sentry.js
+++ app/main/sentry.js
@@ -1,3 +1,3 @@
-import * as Sentry from '@sentry/electron';
+const { init } = require('@sentry/electron/dist/renderer');
 
-Sentry.init({dsn: 'https://cbea024b06984b9ebb56cffce53e4d2f@sentry.io/1483893'});
+init({dsn: 'https://cbea024b06984b9ebb56cffce53e4d2f@sentry.io/1483893'});
