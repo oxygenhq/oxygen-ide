e8eb665aa37345ec554fba3553fb09a4c5f63567
romovs <romovs@gmail.com>
2019-07-28 10:20:23 +0300

    Additional fixes to Java version check

--- app/renderer/services/JavaService.js
+++ app/renderer/services/JavaService.js
@@ -3,19 +3,30 @@ export const JAVA_BAD_VERSION = 'JAVA_BAD_VERSION';
 
 const javaversion = (callback) => {
   try {
-    const spawn = require('child_process').spawn('java', ['-version']);
-    spawn.on('error', function(err){
-        return callback(err, null);
+    var output = '';
+    const cp = require('child_process').spawn('java', ['-version']);
+    cp.on('error', (err) => {
+      console.error('java -version child process error. ', err);
+      return callback(null);
     })
-    spawn.stderr.on('data', function(data) {
+    cp.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+    cp.stderr.on('data', (data) => {
+      output += data.toString();
+    });
+    cp.on('close', (code) => {
       // Java 8 or lower: 1.6.0_23, 1.7.0, 1.7.0_80, 1.8.0_211
       // Java 9 or higher: 9.0.1, 11.0.4, 12, 12.0.1
-      var matches = data.toString().match(/"(\d+\.?\d+)[\._\-\d+]*"/)
+      var matches = output.match(/"(\d+\.?\d+)[\._\-\d+]*"/)
       let javaVersion = matches ? matches[1] : null;
-      return callback(null, javaVersion);
+      if (!javaVersion) {
+        console.error('java -version mismatch error:\n' + output);
+      }
+      return callback(javaVersion);
     });
   } catch(e){
-    callback('catch', e);
+    console.error('java -version spawn error. ', e);
   }
 }
 
@@ -25,8 +36,7 @@ export default class JavaService {
 
   checkJavaVersion() {
     if(window && window.dispatch){
-      javaversion(function(err,version){
-
+      javaversion((version) => {
         if (version) {
           var ver = version.split('.');
           if (ver[0] == 1 && ver[1] != 8 /*lower than 1.8*/ || ver[0] > 10 /*higher than 10*/) {
@@ -39,7 +49,7 @@ export default class JavaService {
           }
         } else {
           window.dispatch({
-              type: JAVA_NOT_FOUND
+            type: JAVA_NOT_FOUND
           });
         }
       })
