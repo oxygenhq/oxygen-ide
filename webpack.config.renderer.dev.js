fe4b72c5d12d853a8fe92a4baa8baa3d98493c52
romovs <romovs@gmail.com>
2019-09-13 11:12:33 +0300

    Bump dependencies

--- webpack.config.renderer.dev.js
+++ webpack.config.renderer.dev.js
@@ -191,7 +191,7 @@ export default merge.smart(baseConfig, {
       // EOT Font
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: 'file-loader',
+        use: 'url-loader',
       },
       // SVG Font
       {
