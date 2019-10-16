0c25d99bc9f57abc5c7da1915a17b781dbe351f0
romovs <romovs@gmail.com>
2019-10-16 19:06:11 +0300

    Adjust device discovery error message wording

--- app/renderer/components/dialogs/XCodeDialog.jsx
+++ app/renderer/components/dialogs/XCodeDialog.jsx
@@ -1,6 +1,5 @@
 import React, { PureComponent, Fragment } from 'react';
 import { Modal, Button } from 'antd';
-import electron from 'electron';
 
 export default class XCodeDialog extends PureComponent {
   close = () => {
@@ -8,38 +7,31 @@ export default class XCodeDialog extends PureComponent {
       this.props.clean();
     }
   }
-
-  processLink = () => {
-    const oxygenUrl = 'http://appium.io/docs/en/drivers/ios-xcuitest-real-devices/';
-    electron.shell.openExternal(oxygenUrl);
-    this.close();
-  }
   
   render() {
     return (
       <Modal
-        title="Run tests support"
-        width={400}
+        title="Device Discovery Service"
+        width={480}
         visible={true}
         onCancel={this.close}
         footer={(
           <Fragment>
             <Button
-              type="primary"
-              onClick={this.processLink}
-            >
-              Read More
-            </Button>
-            <Button
               onClick={this.close}
             >
-              Skip
+              Ok
             </Button>
           </Fragment>
         )}
       >
         <div>
-          { 'You need to install XCode in order to run tests on iOS devices or emulators. Please click "Read More" button for  more details."' }
+          { 'Could not find the instruments binary.' }
+          <br />
+          { 'Please ensure `xcrun -find instruments` can locate it.' }
+          <br />
+          <br />
+          { 'Possible reasons for this could be: Xcode not being installed or license not accepted for `xcrun`.' }
         </div>
       </Modal>
     );
