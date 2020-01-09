7f7c129c3cd1f9295639e9b552a2cd75567fad1c
Savka Oleg <savkaoleg1996@gmail.com>
2020-01-09 12:55:06 +0200

    Fix replace after find bug

--- app/renderer/containers/TextEditor/TextEditor.jsx
+++ app/renderer/containers/TextEditor/TextEditor.jsx
@@ -58,124 +58,136 @@ const getVisible = (file, activeFile, activeFileName) => {
 };
 
 export default class TextEditor extends React.Component<Props> {
-  props: Props;
+    props: Props;
 
-  constructor(props) {
-      super(props);
-      // keeps reference to Rxjs subscriptions  
-      this.subscriptions = {};
-      // subscribe to the relevant subjects
-      this.subscriptions['EDITOR.TRIGGER'] = editorSubjects['EDITOR.TRIGGER'].subscribe(::this.onEditorCallTrigger);
-      this.subscriptions['FILE.RENAMED'] = fileSubjects['FILE.RENAMED'].subscribe(::this.onFileRenamed);
-      // keeps references to all open editors
-      this.editors = {};
-  }
+    constructor(props) {
+        super(props);
+        // keeps reference to Rxjs subscriptions  
+        this.subscriptions = {};
+        // subscribe to the relevant subjects
+        this.subscriptions['EDITOR.TRIGGER'] = editorSubjects['EDITOR.TRIGGER'].subscribe(::this.onEditorCallTrigger);
+        this.subscriptions['FILE.RENAMED'] = fileSubjects['FILE.RENAMED'].subscribe(::this.onFileRenamed);
+        // keeps references to all open editors
+        this.editors = {};
+        this.lastTrigger = null;
+    }
 
-  componentWillUnmount() {
-      if (this.subscriptions['EDITOR.TRIGGER']) {
-          this.subscriptions['EDITOR.TRIGGER'].unsubscribe();
-      }
-      if (this.subscriptions['FILE.RENAMED']) {
-          this.subscriptions['FILE.RENAMED'].unsubscribe();
-      }
-  }
+    componentWillUnmount() {
+        if (this.subscriptions['EDITOR.TRIGGER']) {
+            this.subscriptions['EDITOR.TRIGGER'].unsubscribe();
+        }
+        if (this.subscriptions['FILE.RENAMED']) {
+            this.subscriptions['FILE.RENAMED'].unsubscribe();
+        }
+    }
 
-  onEditorCallTrigger(payload = {}) {
-      const { activeFile } = this.props;
-      const { trigger } = payload;
-      const editor = this.editors[activeFile];
+    onEditorCallTrigger(payload = {}) {
+        const { activeFile } = this.props;
+        const { trigger } = payload;
+        const editor = this.editors[activeFile];
 
-      if (editor && typeof editor.trigger === 'function') {
-          editor.trigger(trigger);
-      }
-  }
+        if(this.lastTrigger && typeof this.lastTrigger === 'string' && ['find', 'replace'].includes(this.lastTrigger)){
+            try {
+                const closeTriggerLine = 'closeFindWidget';
+                editor.trigger(closeTriggerLine);
+                this.lastTrigger = null;
+            } catch(e){
+                console.log('lastTrigger e', e);
+            }
+        }
 
-  onFileRenamed(payload = {}) {
-      const { newPath, oldPath } = payload;
-      if (!newPath || !oldPath) {
-          return; // ignore invalid payload
-      }
-      // if the renamed file had a reference to an editor, make sure to change it's key in 'editors' hash, from old to the new one
-      if (this.editors.hasOwnProperty(oldPath)) {
-          const editorRef = this.editors[oldPath];
-          delete this.editors[oldPath];
-          this.editors[newPath] = editorRef;
-      }
-  }
+        if (editor && typeof editor.trigger === 'function') {
+            editor.trigger(trigger);
+            this.lastTrigger = trigger;
+        }
+    }
 
-  handleValueChange(filePath, value, name) {
-      this.props.onContentUpdate(filePath, value, name);
-  }
+    onFileRenamed(payload = {}) {
+        const { newPath, oldPath } = payload;
+        if (!newPath || !oldPath) {
+            return; // ignore invalid payload
+        }
+        // if the renamed file had a reference to an editor, make sure to change it's key in 'editors' hash, from old to the new one
+        if (this.editors.hasOwnProperty(oldPath)) {
+            const editorRef = this.editors[oldPath];
+            delete this.editors[oldPath];
+            this.editors[newPath] = editorRef;
+        }
+    }
 
-  handleSelectionChange(filePath, selectedText) {
-      // TODO: add here handler for selection-related actions
-  }
+    handleValueChange(filePath, value, name) {
+        this.props.onContentUpdate(filePath, value, name);
+    }
 
-  render() {
-      const { 
-          activeFile, 
-          openFiles, 
-          editorReadOnly, 
-          fontSize,
-          activeFileName
-      } = this.props;
-      const self = this;
+    handleSelectionChange(filePath, selectedText) {
+        // TODO: add here handler for selection-related actions
+    }
 
-      if(activeFile === 'welcome'){
-          return (<Landing/>);
-      }
+    render() {
+        const { 
+            activeFile, 
+            openFiles, 
+            editorReadOnly, 
+            fontSize,
+            activeFileName
+        } = this.props;
+        const self = this;
 
-      return (
-          <Fragment>
-              { openFiles.map( file => {
-                  // NOTE: file.path might be null when 'render' method is called in the middle of file rename process. 
-                  // In this case, we will just ignore render method as it will be shortly called again with the updated file path.
-                  if (!file || !file.path) {
-                      return null;
-                  }
+        if(activeFile === 'welcome'){
+            return (<Landing/>);
+        }
 
-                  const language = SupportedExtensions[file.ext] || DEFAULT_EDITOR_LANGUAGE;
-                  // file.language
+        return (
+            <Fragment>
+                { openFiles.map( file => {
+                    // NOTE: file.path might be null when 'render' method is called in the middle of file rename process. 
+                    // In this case, we will just ignore render method as it will be shortly called again with the updated file path.
+                    if (!file || !file.path) {
+                        return null;
+                    }
 
-                  return (
-                      <MonacoEditor
-                          ref={(ref) => { self.editors[file.path] = ref; }}
-                          key={getKey(file)}
-                          value={file.content}
-                          language={language}
-                          activeLine={file.activeLine}
-                          visible={getVisible(file, activeFile, activeFileName)}
-                          editorReadOnly={editorReadOnly}
-                          fontSize={fontSize}
-                          breakpoints={file.breakpoints || []}
-                          onBreakpointsUpdate={(bps) => this.props.onBreakpointsUpdate(file.path, bps, file.name)}
-                          onValueChange={(bps) => ::this.handleValueChange(file.path, bps, file.name)}
-                          onSelectionChange={(bps) => ::this.handleSelectionChange(file.path, bps)}
-                      />
-                  );
-              })}
-              {(!activeFile || openFiles.length === 0) && (
-                  <div className="noFilesPlaceholder">
-                      <div>
-                          <Icon type="inbox" />
-                          <p>You have no active opened files</p>
-                          <p>Open a folder first, then select a file or create a new file</p>
-                          { process.platform === 'win32' || process.platform === 'linux' ?
-                              <div>
-                                  <p><b>Ctrl + O</b> - Open folder</p>
-                                  <p><b>Ctrl + N</b> - New file</p>
-                              </div>
-                              : 
-                              <div>
-                                  <p><b>Command + O</b> - Open folder</p>
-                                  <p><b>Command + N</b> - New file</p>
-                              </div>
-                          }
-                      </div>
-                  </div>
-              )}
-          </Fragment>
-      );
-  }
+                    const language = SupportedExtensions[file.ext] || DEFAULT_EDITOR_LANGUAGE;
+                    // file.language
+
+                    return (
+                        <MonacoEditor
+                            ref={(ref) => { self.editors[file.path] = ref; }}
+                            key={getKey(file)}
+                            value={file.content}
+                            language={language}
+                            activeLine={file.activeLine}
+                            visible={getVisible(file, activeFile, activeFileName)}
+                            editorReadOnly={editorReadOnly}
+                            fontSize={fontSize}
+                            breakpoints={file.breakpoints || []}
+                            onBreakpointsUpdate={(bps) => this.props.onBreakpointsUpdate(file.path, bps, file.name)}
+                            onValueChange={(bps) => ::this.handleValueChange(file.path, bps, file.name)}
+                            onSelectionChange={(bps) => ::this.handleSelectionChange(file.path, bps)}
+                        />
+                    );
+                })}
+                {(!activeFile || openFiles.length === 0) && (
+                    <div className="noFilesPlaceholder">
+                        <div>
+                            <Icon type="inbox" />
+                            <p>You have no active opened files</p>
+                            <p>Open a folder first, then select a file or create a new file</p>
+                            { process.platform === 'win32' || process.platform === 'linux' ?
+                                <div>
+                                    <p><b>Ctrl + O</b> - Open folder</p>
+                                    <p><b>Ctrl + N</b> - New file</p>
+                                </div>
+                                : 
+                                <div>
+                                    <p><b>Command + O</b> - Open folder</p>
+                                    <p><b>Command + N</b> - New file</p>
+                                </div>
+                            }
+                        </div>
+                    </div>
+                )}
+            </Fragment>
+        );
+    }
 }
 
