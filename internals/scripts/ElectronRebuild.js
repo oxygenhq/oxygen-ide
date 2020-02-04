f60b040e96dd8f1830ca18a097c5ae9859a31094
romovs <romovs@gmail.com>
2020-02-04 16:17:08 +0200

    Fix electron rebuild on Linux

--- internals/scripts/ElectronRebuild.js
+++ internals/scripts/ElectronRebuild.js
@@ -4,7 +4,7 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 import dependencies from '../../app/package.json';
 import rimraf from 'rimraf';
-var electronLocator = require('electron-rebuild/lib/src/./electron-locator');
+import electronPackage  from '../../node_modules/electron/./package.json';
 import nodeAbi from 'node-abi';
 
 const nodeModulesPath =
@@ -31,11 +31,10 @@ if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(nodeModulesPath)
     execSync(cmd, {
         cwd: path.join(__dirname, '..', '..', 'app')
     });
-  
+
     // on linux, append "glibc" suffix to the Fibers binaries folder. see relevant entry in Confluence for more details.
     if (process.platform === 'linux') {
-        const electronPath = electronLocator.locateElectronModule();
-        const electronVersion = require(path.join(electronPath, 'package.json')).version;
+        const electronVersion = electronPackage.version;
         const abi = nodeAbi.getAbi(electronVersion, 'electron');
         const fibersOrinal = path.join(nodeModulesPath, 'fibers', 'bin', process.platform + '-' + process.arch + '-' + abi);
         const fibersFinal = path.join(nodeModulesPath, 'fibers', 'bin', process.platform + '-' + process.arch + '-' + abi + '-glibc');
