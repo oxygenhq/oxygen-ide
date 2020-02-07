5b2260381c0055b0f510fdbbff83fcbc94d3bc54
Savka Oleg <savkaoleg1996@gmail.com>
2020-02-07 16:04:16 +0200

    OI-744 Oxygen welcome page change the URL link for Video tutorials

--- app/renderer/components/Landing.js
+++ app/renderer/components/Landing.js
@@ -24,7 +24,7 @@ export default class Landing extends React.PureComponent {
           <div className="landing-container">
               <div className="landing-inner">
                   <h1 className="landing-title">Welcome to Oxygen</h1>
-                  <p className="landing-text">To see an online tour of Oxygen click <a href="http://docs.oxygenhq.org/about-video-tutorials.html" onClick={this.processLink}>here</a></p>
+                  <p className="landing-text">To see an online tour of Oxygen click <a href="https://docs.oxygenhq.org/about/getting-started-videos" onClick={this.processLink}>here</a></p>
                   <p className="landing-text">To read an online guide of Oxygen click <a href="http://docs.oxygenhq.org/getting-started-web/introduction" onClick={this.processLink}>here</a></p>
                   <p className="landing-text">To learn about Cloudbeat click <a href="http://cloudbeat.io/" onClick={this.processLink}>here</a></p>
                   <div className="videoWrapper">
